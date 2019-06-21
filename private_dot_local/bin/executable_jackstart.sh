b1335f926cc42da2faf6ce9be49d00220abfcee8
James Ravn <james@r-vn.org>
2019-06-21 11:42:23 +0100

    Update

--- private_dot_local/bin/executable_jackstart.sh
+++ private_dot_local/bin/executable_jackstart.sh
@@ -2,10 +2,9 @@
 
 # pacman -S jack2 pulseaudio-jack qjackctl non-session-manager
 # start up jack
-jack_control start
 jack_control ds alsa
-jack_control dps device hw:2
+jack_control dps device hw:$1
 jack_control dps rate 44100
 jack_control dps nperiods 3
 jack_control dps period 64
-qjackctl &
+jack_control start
