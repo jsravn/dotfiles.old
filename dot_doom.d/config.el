bb3b6676ff0e1c5cd1b5f923e18cd470c4918867
James Ravn <james@r-vn.org>
2019-06-25 12:50:00 +0100

    Move env vars into .profile so they load in GUI

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -3,6 +3,7 @@
 ;; Place your private configuration here
 ;; Change doom theme
 (setq doom-theme 'doom-one)
+(setq doom-font (font-spec :family "DejaVu Sans Mono"))
 
 ;; Add projectile projects
 (setq projectile-project-search-path '("~/lightbend/" "~/devel/"))
@@ -12,22 +13,22 @@
 (setq select-enable-clipboard t)
 
 ;; Scala mode
-(def-package! scala-mode
-  :mode "\\.s\\(cala\\|bt\\)$")
-(def-package! sbt-mode
-  :commands sbt-start sbt-command
-  :config
-  ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
-  ;; allows using SPACE when in the minibuffer
-  (substitute-key-definition
-   'minibuffer-complete-word
-   'self-insert-command
-   minibuffer-local-completion-map))
-(def-package! lsp-mode
-  ;; Optional - enable lsp-mode automatically in scala files
-  :hook (scala-mode . lsp)
-  :config (setq lsp-prefer-flymake nil))
-(def-package! lsp-ui)
-(def-package! company-lsp)
-(use-package flycheck
-  :init (global-flycheck-mode))
+;; (def-package! scala-mode
+;;   :mode "\\.s\\(cala\\|bt\\)$")
+;; (def-package! sbt-mode
+;;   :commands sbt-start sbt-command
+;;   :config
+;;   ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
+;;   ;; allows using SPACE when in the minibuffer
+;;   (substitute-key-definition
+;;    'minibuffer-complete-word
+;;    'self-insert-command
+;;    minibuffer-local-completion-map))
+;; (def-package! lsp-mode
+;;   ;; Optional - enable lsp-mode automatically in scala files
+;;   :hook (scala-mode . lsp)
+;;   :config (setq lsp-prefer-flymake nil))
+;; (def-package! lsp-ui)
+;; (def-package! company-lsp)
+;; (use-package flycheck
+;;   :init (global-flycheck-mode))
