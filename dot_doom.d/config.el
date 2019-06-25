412ead40589ac33f09f92891a0f577dd0e38dd78
James Ravn <james@r-vn.org>
2019-06-25 10:38:22 +0100

    Replace prelude with doom emacs

---
+++ dot_doom.d/config.el
@@ -0,0 +1,33 @@
+;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here
+;; Change doom theme
+(setq doom-theme 'doom-one)
+
+;; Add projectile projects
+(setq projectile-project-search-path '("~/lightbend/" "~/devel/"))
+
+;; Clipboard stuff
+(setq select-enable-primary t)
+(setq select-enable-clipboard t)
+
+;; Scala mode
+(def-package! scala-mode
+  :mode "\\.s\\(cala\\|bt\\)$")
+(def-package! sbt-mode
+  :commands sbt-start sbt-command
+  :config
+  ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
+  ;; allows using SPACE when in the minibuffer
+  (substitute-key-definition
+   'minibuffer-complete-word
+   'self-insert-command
+   minibuffer-local-completion-map))
+(def-package! lsp-mode
+  ;; Optional - enable lsp-mode automatically in scala files
+  :hook (scala-mode . lsp)
+  :config (setq lsp-prefer-flymake nil))
+(def-package! lsp-ui)
+(def-package! company-lsp)
+(use-package flycheck
+  :init (global-flycheck-mode))
---
+++ dot_doom.d/packages.el
@@ -0,0 +1,13 @@
+;; -*- no-byte-compile: t; -*-
+;;; ~/.doom.d/packages.el
+
+;;; Examples:
+;; (package! some-package)
+;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
+;; (package! builtin-package :disable t)
+
+(package! scala-mode)
+(package! sbt-mode)
+(package! lsp-mode)
+(package! lsp-ui)
+(package! company-lsp)
