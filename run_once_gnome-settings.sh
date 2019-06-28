e7b9c138c97257501a6f600573c6a9c71c83c1f4
James Ravn <james@r-vn.org>
2019-06-28 14:43:55 +0100

    Add gnome settings script

---
+++ run_once_gnome-settings.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Disable keys which conflict with IntelliJ, Discord, etc.
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['']"
+# Speed up key rate.
+gsettings set org.gnome.desktop.peripherals.keyboard delay 225
+gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 30
