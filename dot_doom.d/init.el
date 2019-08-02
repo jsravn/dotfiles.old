36610851bde748ac58e0503791ebbc8c231814bf
James Ravn <james@r-vn.org>
2019-08-02 15:06:51 +0100

    Make +popup less liberal with what is closeable via ESC

--- dot_doom.d/init.el
+++ dot_doom.d/init.el
@@ -31,7 +31,7 @@
        ;;neotree           ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
        (popup            ; tame sudden yet inevitable temporary windows
-        +all             ; catch all popups that start with an asterix
+       ;;+all             ; catch all popups that start with an asterix
         +defaults)       ; default popup rules
        ;;pretty-code       ; replace bits of code with pretty symbols
        ;;tabbar            ; FIXME an (incomplete) tab bar for Emacs
