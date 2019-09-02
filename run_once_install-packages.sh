3d9a297165297df63f97a6debe48e550342292d3
James Ravn <james@r-vn.org>
2019-09-02 21:14:08 +0100

    Add pyenv

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -2,11 +2,12 @@
 
 set -eu
 
-sudo pacman -Sy --noconfirm \
+sudo pacman -Sy --noconfirm --needed \
     ripgrep \
     zsh \
     chromium \
     emacs \
     editorconfig-core-c \
     fd \
-    fzf
+    fzf \
+    pyenv
