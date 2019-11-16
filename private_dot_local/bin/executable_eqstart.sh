2b075bd6f73e9a379119252a8960cd604263b243
James Ravn <james@r-vn.org>
2019-11-16 20:23:00 +0000

    add ladspa_eq

---
+++ private_dot_local/bin/executable_eqstart.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -eu
+
+# needs dsp setup - see https://www.reddit.com/r/headphones/comments/abi6gp/linux_system_wide_eq/
+pacmd load-module module-ladspa-sink sink_name=dsp master=$1 plugin=ladspa_dsp label=ladspa_dsp
