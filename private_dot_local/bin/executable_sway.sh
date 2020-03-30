738f96a5d875833d6a831fac8302c6a652531223
James Ravn <james@r-vn.org>
2020-03-30 18:29:54 +0100

    Fix java apps in sway

--- private_dot_local/bin/executable_sway.sh
+++ private_dot_local/bin/executable_sway.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 eval $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh 2>/dev/null)
 export SSH_AUTH_SOCK
-exec sway > ~/.sway.txt
+export _JAVA_AWT_WM_NONREPARENTING=1
+exec sway >~/.sway.out 2>~/.sway.err
