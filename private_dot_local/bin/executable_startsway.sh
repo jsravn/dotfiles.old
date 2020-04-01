c8f4eef38b738084b23dedb32409655cc703920f
James Ravn <james@r-vn.org>
2020-04-01 19:03:39 +0100

    More sway powerup

--- private_dot_local/bin/executable_startsway.sh
+++ private_dot_local/bin/executable_startsway.sh
@@ -2,4 +2,5 @@
 eval $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh 2>/dev/null)
 export SSH_AUTH_SOCK
 export _JAVA_AWT_WM_NONREPARENTING=1
+dbus-update-activation-environment --systemd DBUS_SESSION_BUS_ADDRESS DISPLAY XAUTHORITY
 exec sway >~/.sway.out 2>~/.sway.err
