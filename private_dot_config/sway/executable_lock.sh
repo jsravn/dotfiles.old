30a2149e2684cbb7a6a8f6b7a01215eaf87432c4
James Ravn <james@r-vn.org>
2020-04-01 20:18:00 +0100

    More sway tweaks

--- private_dot_config/sway/executable_lock.sh
+++ private_dot_config/sway/executable_lock.sh
@@ -3,4 +3,3 @@
 grim ~/.cache/screen_locked.png
 mogrify -scale 5% -scale 2000% ~/.cache/screen_locked.png
 swaylock -f -i ~/.cache/screen_locked.png
-sleep 60; pgrep swaylock && swaymsg 'output * dpms off'
--- private_dot_local/bin/executable_startsway.sh
+++ private_dot_local/bin/executable_startsway.sh
@@ -3,4 +3,4 @@ eval $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh 2>/
 export SSH_AUTH_SOCK
 export _JAVA_AWT_WM_NONREPARENTING=1
 dbus-update-activation-environment --systemd DBUS_SESSION_BUS_ADDRESS DISPLAY XAUTHORITY
-exec sway >~/.sway.out 2>~/.sway.err
+exec sway --unsupported-gpu >~/.sway.out 2>~/.sway.err
