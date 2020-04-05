4492596a95b89781a9aad38cfe0133641d9ca040
James Ravn <james@r-vn.org>
2020-04-05 12:54:52 +0100

    Fix swaylock on suspend

--- private_dot_config/sway/executable_lock.sh
+++ private_dot_config/sway/executable_lock.sh
@@ -1,5 +1,6 @@
-#!/bin/sh -e
+#!/bin/sh
 
-grim ~/.cache/screen_locked.png
-mogrify -scale 5% -scale 2000% ~/.cache/screen_locked.png
-swaylock -f -i ~/.cache/screen_locked.png
+if grim ~/.cache/screen_locked.png; then
+    mogrify -scale 5% -scale 2000% ~/.cache/screen_locked.png
+fi
+exec swaylock -f -i ~/.cache/screen_locked.png
