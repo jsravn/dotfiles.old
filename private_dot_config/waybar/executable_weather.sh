125f459ec6885cfaccdee0ba92d4cf60a50e4bf5
James Ravn <james@r-vn.org>
2020-04-28 09:51:45 +0100

    Use noto emojis for weather

--- private_dot_config/waybar/executable_weather.sh
+++ private_dot_config/waybar/executable_weather.sh
@@ -35,34 +35,34 @@ temperature=$(echo ${weather[2]} | sed -E 's/([[:digit:]]+)\.\./\1 to /g')
 # https://fontawesome.com/icons?s=solid&c=weather
 case $(echo ${weather[1]##*,} | tr '[:upper:]' '[:lower:]') in
 "clear" | "sunny")
-    condition="☼"
+    condition="🌞"
     ;;
 "partly cloudy" | "haze")
-    condition="⛅"
+    condition="🌤"
     ;;
 "cloudy" | "overcast")
-    condition="☁"
+    condition="🌥"
     ;;
 "mist" | "fog" | "freezing fog")
-    condition="🌁"
+    condition="🌫"
     ;;
-"patchy rain possible" | "patchy light drizzle" | "light drizzle" | "patchy light rain" | "light rain" | "light rain shower" | "rain")
-    condition="⛆"
+"patchy rain possible" | "patchy light drizzle" | "light drizzle" | "patchy light rain" | "light rain" | "light rain shower" | "rain" | "drizzle and rain")
+    condition="🌦"
     ;;
 "moderate rain at times" | "moderate rain" | "heavy rain at times" | "heavy rain" | "moderate or heavy rain shower" | "torrential rain shower" | "rain shower")
-    condition="⛆"
+    condition="🌧"
     ;;
 "patchy snow possible" | "patchy sleet possible" | "patchy freezing drizzle possible" | "freezing drizzle" | "heavy freezing drizzle" | "light freezing rain" | "moderate or heavy freezing rain" | "light sleet" | "ice pellets" | "light sleet showers" | "moderate or heavy sleet showers")
-    condition="❄"
+    condition="🌨"
     ;;
 "blowing snow" | "moderate or heavy sleet" | "patchy light snow" | "light snow" | "light snow showers")
-    condition="❄"
+    condition="🌨"
     ;;
 "blizzard" | "patchy moderate snow" | "moderate snow" | "patchy heavy snow" | "heavy snow" | "moderate or heavy snow with thunder" | "moderate or heavy snow showers")
-    condition="❆"
+    condition="🌨"
     ;;
 "thundery outbreaks possible" | "patchy light rain with thunder" | "moderate or heavy rain with thunder" | "patchy light snow with thunder")
-    condition="⛈"
+    condition="🌩"
     ;;
 *)
     condition=""
