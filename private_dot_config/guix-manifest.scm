7e3abafab8066a1c0239b9e7b9ea8ac6b561692c
James Ravn <james@r-vn.org>
2020-05-15 12:05:34 +0100

    Add initial guix manifest
    For installing packages I use as a user.

---
+++ private_dot_config/guix-manifest.scm
@@ -0,0 +1,15 @@
+(specifications->manifest
+ '("glibc-locales"
+   "fontconfig"
+   "font-dejavu"
+   "font-google-noto"
+   "emacs-next"
+   "sway"
+   "swaylock"
+   "swayidle"
+   "swaybg"
+   "waybar"
+   "ripgrep"
+   "go-github-com-junegunn-fzf"
+   "fd"
+   "zsh"))
